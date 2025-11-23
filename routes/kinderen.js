a0aad18b3188366a84b35ca1fa798234177df3f5
hdktsaam <hannes.dekesel@tsaam.be>
2025-11-23 15:26:30 +0100

    feedback

--- routes/kinderen.js
+++ routes/kinderen.js
@@ -1,16 +1,17 @@
 // zorgt ervoor dat je de nodige date hebt
-const express = require("express")
-const crlKind = require("../controllers/kinderen")
+const express = require("express");
+const crlKind = require("../controllers/kinderen");
 
 // maak router object aan
-const routes = express.Router()
+const routes = express.Router();
 
 // maak de verschillende routs aan
-routes.get("/", crlKind.infoAll)
-routes.get("/:ID", crlKind.getOne)
-routes.put("/addOne", crlKind.addOne)
-routes.put("/addGift", crlKind.addGift)
-routes.post("/verwijderopnaam", crlKind.deleteOnePost)
+// mocht gelijkaardig aan die van geschenk maar zeker ok
+routes.get("/", crlKind.infoAll);
+routes.get("/:ID", crlKind.getOne);
+routes.put("/addOne", crlKind.addOne);
+routes.put("/addGift", crlKind.addGift);
+routes.post("/verwijderopnaam", crlKind.deleteOnePost);
 
 // alles exporteren
-module.exports = routes
+module.exports = routes;
